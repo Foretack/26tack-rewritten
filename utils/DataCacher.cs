3d8aaa29f3b01aa7c62be24213a6c662fc7c7f35
2vitreous <observer.in.a.box@gmail.com>
2022-05-10 18:46:54 +0200

    DataCacher<> instead of "Saving" interface

---
+++ utils/DataCacher.cs
@@ -0,0 +1,16 @@
+﻿namespace _26tack_rewritten.utils;
+public abstract class DataCacher<T>
+{
+    private protected Dictionary<string, CachedPiece> CachedData = new Dictionary<string, CachedPiece>();
+
+    protected void ClearCachedData() { CachedData.Clear(); }
+    protected void CachePiece(string key, CachedPiece item) { CachedData.TryAdd(key, item); }
+    protected CachedPiece? GetCachedPiece(string key)
+    {
+        bool s = CachedData.TryGetValue(key, out CachedPiece? piece);
+        if (s && DateTimeOffset.Now.ToUnixTimeSeconds() - piece!.CachingUnixTime >= piece.MaxCachingTime) return null;
+        return piece;
+    }
+
+    protected record CachedPiece(T Object, long CachingUnixTime, int MaxCachingTime);
+}
