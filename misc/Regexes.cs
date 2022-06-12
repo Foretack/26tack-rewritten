3e3737fa7297784fe7ac5ea061c378015afa6f4d
Foretack <observer.in.a.box@gmail.com>
2022-06-12 10:49:36 +0200

    fix regex

--- misc/Regexes.cs
+++ misc/Regexes.cs
@@ -4,6 +4,6 @@ namespace _26tack_rewritten.misc;
 
 public static class Regexes
 {
-    public static readonly Regex Mention = new Regex(@"\x{20e3}\s?\x{1f6e4}|4s?tac?k|fo(re?[esk]|ur|r)tr?ac?k|129708505|\btest(ing)? ?(guy|individual)|login_unavailable|783267696|occluder", RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromMilliseconds(50));
+    public static readonly Regex Mention = new Regex(@"4s?tac?k|fo(re?[esk]|ur|r)tr?ac?k|129708505|\btest(ing)? ?(guy|individual)|login_unavailable|783267696|occluder", RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromMilliseconds(50));
     public static readonly Regex Racism = new Regex(@"(?:(?:\b(?<![-=\.])|monka)(?:[Nnñ]|[Ii7]V)|η|[\/|]\\[\/|])[\s\.]*?[liI1y!j\/|]+[\s\.]*?(?:[GgbB6934Q🅱qğĜƃ၅5\*][\s\.]*?){2,}(?!arcS|l|Ktlw|ylul|ie217|64|\d? ?times)", RegexOptions.Compiled, TimeSpan.FromMilliseconds(300));
 }
