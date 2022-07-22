e7e3e51beaf431c28b263238e509df96d3ca173b
Foretack <observer.in.a.box@gmail.com>
2022-07-22 18:44:43 +0200

    Use shared Random instead of making new one

--- Utils/Choices.cs
+++ Utils/Choices.cs
@@ -2,8 +2,6 @@
 
 internal static class Choices
 {
-    private static readonly Random Rng = new Random();
-
-    public static T Choice<T>(this IEnumerable<T> en) { return en.ElementAt(Rng.Next(en.Count())); }
-    public static char Choice(this string str) { return str[Rng.Next(str.Length)]; }
+    public static T Choice<T>(this IEnumerable<T> en) { return en.ElementAt(Random.Shared.Next(en.Count())); }
+    public static char Choice(this string str) { return str[Random.Shared.Next(str.Length)]; }
 }
