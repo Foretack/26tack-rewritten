3d3129e83bfd67dc00afb4f5369e1bac02ecfae6
Foretack <observer.in.a.box@gmail.com>
2022-08-21 18:49:39 +0200

    (totally works!!) Discord rpc updates

--- Core/DiscordClient.cs
+++ Core/DiscordClient.cs
@@ -1,4 +1,5 @@
-﻿using Discord;
+﻿using System.Text;
+using Discord;
 using Discord.WebSocket;
 using Serilog;
 using Tack.Handlers;
@@ -8,12 +9,18 @@ internal static class DiscordClient
 {
     #region Properties
     public static bool Connected { get; private set; } = false;
-    public static DiscordSocketClient Client { get; } = new DiscordSocketClient();
+    public static DiscordSocketClient? Client { get; private set; }
+
+    private static readonly DiscordSocketConfig ClientConfig = new()
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildPresences
+    };
     #endregion
 
     #region Initialization
     public static async Task Connect()
     {
+        Client = new DiscordSocketClient(ClientConfig);
         await Client.LoginAsync(TokenType.Bot, Config.Auth.DiscordToken);
         await Client.StartAsync();
         RegisterEvents(Client);
@@ -24,7 +31,9 @@ internal static class DiscordClient
         client.MessageReceived += MessageHandler.OnDiscordMessageReceived;
         client.Connected += OnConnected;
         client.Disconnected += OnDisconnected;
+        client.PresenceUpdated += PresenceUpdated;
     }
+
     #endregion
 
     #region Client events
@@ -40,6 +49,35 @@ internal static class DiscordClient
         Log.Information("[Discord] Connected");
         Connected = true;
         return Task.CompletedTask;
+    }
+
+    private static async Task PresenceUpdated(SocketUser user, SocketPresence arg2, SocketPresence arg3)
+    {
+        if (user.IsBot) return;
+
+        await Task.Run(() =>
+        {
+            var activities = arg2.Activities;
+            foreach (var activity in activities)
+            {
+                if (activity is null) continue;
+
+                var sb = new StringBuilder(user.Username);
+                switch (activity.Type)
+                {
+                    case ActivityType.Listening:
+                        sb.Append($" is listening to: {activity.Details}");
+                        break;
+                    case ActivityType.Streaming:
+                        sb.Append($" is streaming: {activity.Name} ({activity.Details})");
+                        break;
+                    default:
+                        sb.Append($" is playing: {activity.Name} " + (string.IsNullOrEmpty(activity.Details) ? String.Empty : $"({activity.Details})"));
+                        break;
+                }
+                MessageHandler.SendMessage(Config.RelayChannel, $"{user.Username} {activity.Type}: {activity.Name} ({activity.Details}) ");
+            }
+        });
     }
     #endregion
 }
