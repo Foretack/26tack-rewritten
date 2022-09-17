4d6d91786f9677afd714f85296d606bb4e0da527
Foretack <observer.in.a.box@gmail.com>
2022-09-17 19:35:24 +0200

    Add more time related utils

--- Utils/Time.cs
+++ Utils/Time.cs
@@ -5,4 +5,25 @@ internal static class Time
     public static string UntilString(DateTime time) => (time.ToLocalTime() - DateTime.Now).FormatTimeLeft();
     public static TimeSpan Since(DateTime time) => DateTime.Now - time.ToLocalTime();
     public static string SinceString(DateTime time) => (DateTime.Now - time.ToLocalTime()).FormatTimeLeft();
+    public static void Schedule(Action action, DateTime dueTime) => Schedule(action, Until(dueTime));
+    public static void DoEvery(int seconds, Action action) => DoEvery(TimeSpan.FromSeconds(seconds), action);
+
+    public static void Schedule(Action action, TimeSpan dueTime)
+    {
+        Timer? t = null;
+        t = new Timer(async _ => await Task.Run(() =>
+        {
+            action.Invoke();
+            t?.Dispose();
+        }),
+        null, dueTime, Timeout.InfiniteTimeSpan);
+    }
+
+    public static void DoEvery(TimeSpan period, Action action)
+    {
+        System.Timers.Timer timer = new();
+        timer.Interval = period.TotalMilliseconds;
+        timer.Enabled = true;
+        timer.Elapsed += async (_, _) => await Task.Run(() => action.Invoke());
+    }
 }
