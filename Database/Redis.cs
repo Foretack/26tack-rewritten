dfcf065df927f05493ad0c8af98dc71f57b124d8
Foretack <observer.in.a.box@gmail.com>
2022-09-24 09:30:37 +0200

    Initial Redis integration

---
+++ Database/Redis.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using Tack.Utils;
+
+namespace Tack.Database;
+internal static class Redis
+{
+    private static readonly RedisConnection _connection = new("localhost");
+
+    public static async Task<RedisValue> FromRedisAndSet(this string key, RedisValue newValue) => await _connection.Db.StringGetSetAsync(key, newValue);
+    public static async Task<RedisValue> FromRedis(this string key) => await _connection.Db.StringGetAsync(key);
+    public static async Task<RedisValueWithExpiry> FromRedisWithExpiry(this string key) => await _connection.Db.StringGetWithExpiryAsync(key);
+    public static async Task<RedisValue> FromRedisAndSetExpiry(this string key, TimeSpan setExpiry) => await _connection.Db.StringGetSetExpiryAsync(key, setExpiry);
+    public static async Task<RedisValue> FromRedisAndSetExpiry(this string key, DateTime setExpiry) => await _connection.Db.StringGetSetExpiryAsync(key, setExpiry);
+    public static async Task<RedisValue> RedisSet(this string key, RedisValue value) => await _connection.Db.StringSetAsync(key, value);
+    public static async Task<RedisValue> RedisSetExpiring(this string key, RedisValue value, TimeSpan expiry) => await _connection.Db.StringSetAsync(key, value, expiry);
+    public static async Task<RedisValue> RedisSetExpiring(this string key, RedisValue value, DateTime expiry) => await _connection.Db.StringSetAsync(key, value, Time.Until(expiry));
+
+    public static void Subscribe(RedisChannel channel, Action<RedisChannel, RedisValue> handler) => _connection.Sub.Subscribe(channel, handler);
+    public static long Publish(RedisChannel channel, RedisValue message) => _connection.Sub.Publish(channel, message);
+    public static async Task<long> PublishAsync(RedisChannel channel, RedisValue message) => await _connection.Sub.PublishAsync(channel, message);
+}
---
+++ Database/RedisConnection.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+using StackExchange.Redis;
+
+namespace Tack.Database;
+internal sealed class RedisConnection
+{
+    public IDatabaseAsync Db { get; private set; }
+    public ISubscriber Sub { get; private set; }
+
+    public RedisConnection(string host)
+    {
+        var con = ConnectionMultiplexer.Connect(host);
+        Log.Debug("Redis connection established");
+        Db = con.GetDatabase();
+        Sub = con.GetSubscriber();
+    }
+}
