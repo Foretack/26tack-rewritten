7cf717de43455bf44542add2768d4f93b35b9255
Foretack <observer.in.a.box@gmail.com>
2022-09-29 12:33:25 +0200

    Add shard status to ping command

--- Commands/BaseSet/Ping.cs
+++ Commands/BaseSet/Ping.cs
@@ -1,4 +1,5 @@
-﻿using Tack.Handlers;
+﻿using Tack.Database;
+using Tack.Handlers;
 using Tack.Misc;
 using Tack.Models;
 using Tack.Nonclass;
@@ -14,16 +15,22 @@ internal sealed class Ping : Command
         aliases: new string[] { "pong", "peng", "pang", "pung" }
     );
 
-    public override Task Execute(CommandContext ctx)
+    public override async Task Execute(CommandContext ctx)
     {
         string user = ctx.IrcMessage.DisplayName;
         string channel = ctx.IrcMessage.Channel;
         double latency = DateTimeOffset.Now.ToUnixTimeMilliseconds() - double.Parse(ctx.IrcMessage.TmiSentTs);
-        TimeSpan uptime = DateTime.Now - C.StartupTime;
+        string uptime = Time.SinceString(C.StartupTime);
+        string? shardStatus = null;
 
+        Redis.Subscribe("shard:manage").OnMessage(x =>
+        {
+            if (x.Message.ToString().Contains("active,")) shardStatus = x.Message;
+        });
+        await Redis.PublishAsync("shard:manage", "PING");
         MessageHandler.SendMessage(channel, $"{string.Join($" {user} ", RandomReplies.PingReplies.Choice())} " +
             $"● {latency}ms " +
-            $"■ Uptime: {uptime.FormatTimeLeft()}");
-        return Task.CompletedTask;
+            $"● Uptime: {uptime} ● Shard status: {shardStatus ?? "unknown"}");
+
     }
 }
