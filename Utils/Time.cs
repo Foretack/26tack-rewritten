b024e0890a0e40338bb98891cba6a0c607e60b20
Foretack <observer.in.a.box@gmail.com>
2022-10-01 16:33:52 +0200

    Add async equivelants of `Time.Schedule()` and `Time.DoEvery()`

--- Utils/Time.cs
+++ Utils/Time.cs
@@ -6,6 +6,7 @@ internal static class Time
     public static TimeSpan Since(DateTime time) => DateTime.Now - time.ToLocalTime();
     public static string SinceString(DateTime time) => (DateTime.Now - time.ToLocalTime()).FormatTimeLeft();
     public static void Schedule(Action action, DateTime dueTime) => Schedule(action, Until(dueTime));
+    public static void Schedule(Func<Task> task, DateTime dueTime) => Schedule(task, Until(dueTime));
     public static void DoEvery(int seconds, Action action) => DoEvery(TimeSpan.FromSeconds(seconds), action);
 
     public static void Schedule(Action action, TimeSpan dueTime)
@@ -16,11 +17,26 @@ internal static class Time
             return;
         }
         Timer? t = null;
-        t = new Timer(async _ => await Task.Run(() =>
+        t = new Timer(_ =>
         {
             action.Invoke();
             t?.Dispose();
-        }),
+        },
+        null, dueTime, Timeout.InfiniteTimeSpan);
+    }
+    public static void Schedule(Func<Task> task, TimeSpan dueTime)
+    {
+        if (dueTime.TotalMilliseconds < 0)
+        {
+            Log.Warning($"{nameof(Schedule)} attempted to schedule something in the past ({dueTime})");
+            return;
+        }
+        Timer? t = null;
+        t = new Timer(async _ =>
+        {
+            await task.Invoke();
+            t?.Dispose();
+        },
         null, dueTime, Timeout.InfiniteTimeSpan);
     }
 
@@ -35,5 +51,17 @@ internal static class Time
         timer.Interval = period.TotalMilliseconds;
         timer.Enabled = true;
         timer.Elapsed += async (_, _) => await Task.Run(() => action.Invoke());
+    }
+    public static void DoEvery(TimeSpan period, Func<Task> task)
+    {
+        if (period.TotalMilliseconds < 0)
+        {
+            Log.Warning($"{nameof(DoEvery)} attempted to set negative period ({period})");
+            return;
+        }
+        System.Timers.Timer timer = new();
+        timer.Interval = period.TotalMilliseconds;
+        timer.Enabled = true;
+        timer.Elapsed += async (_, _) => await task.Invoke();
     }
 }
