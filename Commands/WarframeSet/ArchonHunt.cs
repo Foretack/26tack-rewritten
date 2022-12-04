8b270442198a5c4410658a8c99602bf8e9676645
Foretack <observer.in.a.box@gmail.com>
2022-12-04 13:24:21 +0100

    Remove commented out code in ArchonHunt.cs

--- Commands/WarframeSet/ArchonHunt.cs
+++ Commands/WarframeSet/ArchonHunt.cs
@@ -17,18 +17,6 @@ internal sealed class ArchonHunt : Command
         string user = ctx.IrcMessage.DisplayName;
         string channel = ctx.IrcMessage.Channel;
 
-        //ArchonData archonData = await "warframe:archonhunt".GetOrCreate<ArchonData>(async () =>
-        //{
-        //    var r = await ExternalAPIHandler.WarframeStatusApi<ArchonData>("archonHunt", timeout: 10);
-        //    if (!r.Success)
-        //    {
-        //        MessageHandler.SendMessage(channel, $"@{user}, ⚠ Request error! {r.Exception.Message}");
-        //        return default!;
-        //    }
-        //    return r.Value;
-        //}, true);
-        //if (archonData is null || archonData == default(ArchonData)) return;
-
         var archonDataCache = await Redis.Cache.TryGetObjectAsync<ArchonData>("warframe:archonhunt");
         if (!archonDataCache.keyExists)
         {
