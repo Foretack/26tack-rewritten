a896bc9b3d2f3380d3d6b5c58864ad53b747f3fb
Foretack <observer.in.a.box@gmail.com>
2022-12-07 02:13:34 +0100

    #4

--- Core/Program.cs
+++ Core/Program.cs
@@ -29,7 +29,6 @@ public static class Program
             .WriteTo.Discord(AppConfigLoader.Config.LoggingWebhookUrl, restrictedToMinimumLevel: LogEventLevel.Debug)
             .CreateLogger();
 
-        var db = new DbQueries();
 
         StartupTime = DateTime.Now;
 
