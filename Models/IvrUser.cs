c4ee3d1cd2ea15044f46db3c9e63a0bc4c16a875
Foretack <observer.in.a.box@gmail.com>
2023-01-12 20:22:31 +0100

    :/

--- Models/IvrUser.cs
+++ Models/IvrUser.cs
@@ -15,7 +15,7 @@ public record ChatSettings(
 );
 
 public record LastBroadcast(
-    [property: JsonPropertyName("startedAt")] DateTime StartedAt,
+    [property: JsonPropertyName("startedAt")] DateTime? StartedAt,
     [property: JsonPropertyName("title")] string Title
 );
 
@@ -44,14 +44,14 @@ public record IvrUser(
     [property: JsonPropertyName("logo")] string Logo,
     [property: JsonPropertyName("banner")] string Banner,
     [property: JsonPropertyName("verifiedBot")] bool VerifiedBot,
-    [property: JsonPropertyName("createdAt")] DateTime CreatedAt,
-    [property: JsonPropertyName("updatedAt")] DateTime UpdatedAt,
+    [property: JsonPropertyName("createdAt")] DateTime? CreatedAt,
+    [property: JsonPropertyName("updatedAt")] DateTime? UpdatedAt,
     [property: JsonPropertyName("emotePrefix")] string EmotePrefix,
     [property: JsonPropertyName("roles")] Roles Roles,
     [property: JsonPropertyName("badges")] IReadOnlyList<object> Badges,
     [property: JsonPropertyName("chatSettings")] ChatSettings ChatSettings,
     [property: JsonPropertyName("stream")] object Stream,
-    [property: JsonPropertyName("lastBroadcast")] LastBroadcast LastBroadcast,
+    [property: JsonPropertyName("lastBroadcast")] LastBroadcast? LastBroadcast,
     [property: JsonPropertyName("panels")] IReadOnlyList<Panel> Panels
 );
 
