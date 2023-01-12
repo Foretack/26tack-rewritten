b7e071f2a1d1a1a5dcb1cd71d2d295472d7fccb1
Foretack <observer.in.a.box@gmail.com>
2023-01-12 14:07:37 +0100

    Create FixedStack: Stack<T> which doesnt .Push more than `maxSize`

---
+++ Utils/FixedStack.cs
@@ -0,0 +1,16 @@
+﻿namespace Tack.Utils;
+internal sealed class FixedStack<T> : Stack<T>
+{
+    private readonly int _maxSize;
+
+    public FixedStack(int maxSize) : base(maxSize)
+    {
+        _maxSize = maxSize;
+    }
+
+    public new void Push(T item)
+    {
+        if (base.Count >= _maxSize) return;
+        base.Push(item);
+    }
+}
