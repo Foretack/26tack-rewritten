10af5dc5bdde51748d86f1bae8d7fc3830053b2e
Foretack <observer.in.a.box@gmail.com>
2023-02-08 15:33:11 +0100

    Logging

--- Models/User.cs
+++ Models/User.cs
@@ -5,55 +5,57 @@ using HelixUser = TwitchLib.Api.Helix.Models.Users.GetUsers.User;
 namespace Tack.Models;
 public sealed class User
 {
-	public string DisplayName { get; init; }
-	public string Username { get; init; }
-	public string Id { get; init; }
-	public string Type { get; init; }
-	public string BroadcasterType { get; init; }
-	public DateTime CreatedAt { get; init; }
-	public string Description { get; init; }
-	public string Email { get; init; }
-	public string AvatarUrl { get; init; }
-	public string OfflineImageUrl { get; init; }
+    public string DisplayName { get; init; }
+    public string Username { get; init; }
+    public string Id { get; init; }
+    public string Type { get; init; }
+    public string BroadcasterType { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public string Description { get; init; }
+    public string Email { get; init; }
+    public string AvatarUrl { get; init; }
+    public string OfflineImageUrl { get; init; }
 
-	public static async Task<Result<User>> Get(string username)
-	{
-		GetUsersResponse helixUser = await TwitchAPIHandler.Instance.Api.Helix.Users.GetUsersAsync(logins: new() { username });
-		var user = helixUser.Users.FirstOrDefault();
+    public static async Task<Result<User>> Get(string username)
+    {
+        GetUsersResponse helixUser = await TwitchAPIHandler.Instance.Api.Helix.Users.GetUsersAsync(logins: new() { username });
+        var user = helixUser.Users.FirstOrDefault();
 
-		if (user is null)
-		{
-			return new Result<User>(default!, false, new ArgumentOutOfRangeException());
-		}
-		return new Result<User>(new(user), true, default!);
-	}
+        if (user is null)
+        {
+            Log.Error("Failed to get user {user} from Helix", username);
+            return new Result<User>(default!, false, new ArgumentOutOfRangeException());
+        }
+        return new Result<User>(new(user), true, default!);
+    }
 
-	public static async Task<Result<ExtendedChannel>> GetChannel(ChannelHandler.Channel @base)
-	{
-		var baseResult = await Get(@base.Name);
-		if (!baseResult.Success)
-		{
-			return new Result<ExtendedChannel>(default!, false, new ArgumentNullException());
-		}
-		var value = baseResult.Value;
+    public static async Task<Result<ExtendedChannel>> GetChannel(ChannelHandler.Channel @base)
+    {
+        var baseResult = await Get(@base.Name);
+        if (!baseResult.Success)
+        {
+            Log.Error("Failed to get user {user} from Helix", @base.Name);
+            return new Result<ExtendedChannel>(default!, false, new ArgumentNullException());
+        }
+        var value = baseResult.Value;
 
 
-		return new Result<ExtendedChannel>(new(value.DisplayName, value.Username, value.Id, value.AvatarUrl, value.CreatedAt, @base.Priority, @base.Logged), true, default!);
-	}
+        return new Result<ExtendedChannel>(new(value.DisplayName, value.Username, value.Id, value.AvatarUrl, value.CreatedAt, @base.Priority, @base.Logged), true, default!);
+    }
 
-	private User(HelixUser helixUser)
-	{
-		DisplayName = helixUser.DisplayName;
-		Username = helixUser.Login;
-		Id = helixUser.Id;
-		Type = helixUser.Type;
-		BroadcasterType = helixUser.BroadcasterType;
-		CreatedAt = helixUser.CreatedAt;
-		Description = helixUser.Description;
-		Email = helixUser.Email;
-		AvatarUrl = helixUser.ProfileImageUrl;
-		OfflineImageUrl = helixUser.OfflineImageUrl;
-	}
+    private User(HelixUser helixUser)
+    {
+        DisplayName = helixUser.DisplayName;
+        Username = helixUser.Login;
+        Id = helixUser.Id;
+        Type = helixUser.Type;
+        BroadcasterType = helixUser.BroadcasterType;
+        CreatedAt = helixUser.CreatedAt;
+        Description = helixUser.Description;
+        Email = helixUser.Email;
+        AvatarUrl = helixUser.ProfileImageUrl;
+        OfflineImageUrl = helixUser.OfflineImageUrl;
+    }
 }
 
 public sealed record PartialUser(string Displayname, string Username, string ID);
